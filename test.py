b81137b5b28c10500534685949340508d3b0d95f
LLouice <g13920228592@gmail.com>
2020-08-14 11:52:56 +0800

    improve logger

--- test.py
+++ test.py
@@ -2,15 +2,24 @@ from config import config
 from utils import get_logger
 import time
 
+
 def test_template():
     logger = get_logger(config)
-    logger.debug("from test template so mamy balabala balabala.........................")
+    logger.info("this is a info")
+    logger.debug(
+        "from test template so mamy balabala balabala........................."
+    )
+    logger.error("this is a error!")
     # tc = time.perf_counter()
     # time.sleep(2)
     # logger.debug(f"Elapsed Time {time.perf_counter()-tc:.4f}")
     logger.info(f"{'='*42} \n\t {config} \n\t {'='*42}")
-    
+    test_accept_logger(logger)
+
+
+def test_accept_logger(logger):
+    logger.info("from a accept logger!")
+
 
 if __name__ == "__main__":
     test_template()
-    
--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ def get_logger(config):
     import sys
     from loguru import logger
     fmt = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {module}:{function}:{line} \n\t {message}\n"
-    logger.add(config.log_file, level="DEBUG", format=fmt)
+    logger.add(config.log_file+"_{time:YYYY-MM-DD_HH:mm:ss}.log", level="DEBUG", format=fmt)
     logger.add(sys.stderr, level="ERROR", format=fmt)
     return logger
