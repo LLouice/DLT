28ce3a524166081a7bfb9ada17c0f425b11830c8
LLouice <g13920228592@gmail.com>
2020-08-19 15:26:26 +0800

    add log_dict

--- utils.py
+++ utils.py
@@ -4,6 +4,15 @@ def get_logger(config):
     logger.remove()
     fmt = "<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | <lvl>{level}</lvl> | <c>{module}:{function}:{line}</c> \n\t {message}\n"
     logger.add(sys.stdout, level="DEBUG", colorize=True, format=fmt)
-    logger.add(config.log_file+"_{time:YYYY-MM-DD_HH:mm:ss}.log", level="DEBUG", format=fmt)
+    logger.add(config.log_file + "_{time:YYYY-MM-DD_HH:mm:ss}.log",
+               level="DEBUG",
+               format=fmt)
     # logger.add(sys.stderr, level="ERROR", format=fmt)
     return logger
+
+
+def log_dict(logger, log):
+    msg = "\n"
+    for i, (k, v) in enumerate(log.items()):
+        msg += f"\n\t{k} : {v}"
+    logger.info(msg)
